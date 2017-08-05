b2f930f0410167a8b145c252851734dc01908f23
Igor Tkachenko <IATkachenko@gmail.com>
2017-08-05 19:33:30 +0300

    Added base config reader and config file example
    By default we use config.json and supports -c and --config options to
      specify config file. For example:
        node main.js --config config.json.example
    Example of config file shows main options that we are going to support.

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+var commandLine = require('commander');
 var log = require('./lib/logger.js');
+var configFile = require('./lib/config.js');
+ 
+commandLine
+  .version('0.1.0')
+  .option('-c, --config <file>', 'Config file', 'config.json')
+  .parse(process.argv);
+ 
+var config = new configFile(commandLine.config).get();
+
+var crosspost;
+for (crosspost of config.crossPost) {
+
+  log.write(crosspost.from);
+  log.write(crosspost.to);
+ 
+}
